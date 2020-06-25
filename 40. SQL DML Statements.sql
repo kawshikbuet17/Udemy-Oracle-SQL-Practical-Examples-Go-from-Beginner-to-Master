afabe405c816d79ea976547964463e3023aaf395
Kawshik Kumar Paul <44879224+kawshikbuet17@users.noreply.github.com>
2020-06-25 22:36:48 +0600

    Add files via upload

---
+++ 40. SQL DML Statements.sql
@@ -0,0 +1,29 @@
+--INSERT
+
+SELECT * FROM DEPT;
+
+INSERT INTO dept VALUES(50, 'Training', 'Tokyo');
+INSERT INTO dept(deptno, loc) VALUES(60,'Tokyo');
+
+SELECT * FROM dept;
+
+--UPDATE
+
+SELECT * FROM emp;
+
+UPDATE empSET sal = sal + 200 
+WHERE deptno = 10;
+
+SELECT * FROM emp;
+
+
+--DELETE 
+
+SELECT * FROM dept;
+
+DELETE FROM dept
+WHERE deptno in (50, 60);
+
+SELECT * FROM dept;
+
+
